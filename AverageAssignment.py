0553fb81fea25602b062a1fed367bf30de5a5578
Father-Hensley <jhensley0818@gmail.com>
2025-02-10 10:42:09 -0500

    Ave. and Fix.

--- AverageAssignment.py
+++ AverageAssignment.py
@@ -8,3 +8,13 @@
 #Number 2: 11
 #Number 3: 12
 #The sum of 11 and 11 and 12 is 34 and the average is 11.333333333333334.
+print("Pick three numbers and we will find the sum and the average.")
+#Number 1
+A = 23
+#Number 2
+B = 12
+#Number 3
+C = 55
+D = A + B + C
+E = D /3
+print(f'The sum of your choices is {D}, and the average is {E}.')
